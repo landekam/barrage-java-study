0f70e363fe8513a169504ca69d58ca3c386902ac
Matej Landeka <=>
2024-02-19 07:54:47 +0100

    Add trigger for checking available seats

--- src/main/java/com/setronica/eventing/app/TicketService.java
+++ src/main/java/com/setronica/eventing/app/TicketService.java
@@ -2,7 +2,6 @@ package com.setronica.eventing.app;
 
 import com.setronica.eventing.exceptions.BadRequestException;
 import com.setronica.eventing.exceptions.NotFoundException;
-import com.setronica.eventing.persistence.EventSchedule;
 import com.setronica.eventing.persistence.EventScheduleRepository;
 import com.setronica.eventing.persistence.TicketOrder;
 import com.setronica.eventing.persistence.TicketRepository;
@@ -33,40 +32,24 @@ public class TicketService {
     }
 
     public TicketOrder createTicket(TicketOrder ticket) {
-            ticket.setStatus(TicketStatus.BOOKED);
-            List<TicketOrder> tickets = getAll();
-            tickets = tickets.stream().filter(
-                ticketOrder -> ticketOrder.getEventScheduleId().equals(
-                    ticket.getEventScheduleId())).toList();
-            int ticketCounter = 0;
-            for (TicketOrder eventTicket : tickets) {
-                if (eventTicket.getStatus().equals(TicketStatus.SALE) ||
-                    eventTicket.getStatus().equals(TicketStatus.BOOKED)) {
-                    ticketCounter += eventTicket.getAmount();
-                }
-            }
-
-            EventSchedule schedule =
-                eventScheduleRepository.findById(ticket.getEventScheduleId()).orElseThrow(
-                    () -> new NotFoundException(
-                        "Event schedule not found with id=" + ticket.getEventScheduleId()));
-            if (schedule.getAvailableSeats() < ticket.getAmount() + ticketCounter) {
-                throw new BadRequestException("Not enough available seats");
-            }
-            TicketOrder newTicketOrder = ticketRepository.save(ticket);
-            log.info("Created ticket order with id: " + newTicketOrder.getId());
-            return newTicketOrder;
+        ticket.setStatus(TicketStatus.BOOKED);
+        TicketOrder newTicketOrder = ticketRepository.save(ticket);
+        log.info("Created ticket order with id: " + newTicketOrder.getId());
+        return newTicketOrder;
     }
 
     public TicketOrder updateTicket(TicketOrder ticket, Integer id) {
         ticket.setId(id);
-        eventScheduleRepository.findById(ticket.getEventScheduleId()).orElseThrow(() -> new NotFoundException("Event schedule not found with id=" + ticket.getEventScheduleId()));
-        TicketOrder ticketToUpdate =  getById(ticket.getId());
-        if (ticketToUpdate.getStatus().equals(TicketStatus.REFUNDED) || ticketToUpdate.getStatus().equals(TicketStatus.SALE)) {
-          throw new BadRequestException("Ticket order with id " + ticketToUpdate.getId() + " already payed for and cannot be changed");
+        eventScheduleRepository.findById(ticket.getEventScheduleId()).orElseThrow(
+                () -> new NotFoundException("Event schedule not found with id=" + ticket.getEventScheduleId()));
+        TicketOrder ticketToUpdate = getById(ticket.getId());
+        if (ticketToUpdate.getStatus().equals(TicketStatus.REFUNDED)
+                || ticketToUpdate.getStatus().equals(TicketStatus.SALE)) {
+            throw new BadRequestException(
+                    "Ticket order with id " + ticketToUpdate.getId() + " already payed for and cannot be changed");
         }
         ticket.setStatus(ticketToUpdate.getStatus());
-        TicketOrder updatedTicketOrder =  ticketRepository.save(ticket);
+        TicketOrder updatedTicketOrder = ticketRepository.save(ticket);
         log.info("Updated ticket order with id: " + updatedTicketOrder.getId());
         return updatedTicketOrder;
     }
